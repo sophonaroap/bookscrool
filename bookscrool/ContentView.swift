ff00747477a41c5827f9e1d8e999261c393e0167
sophie <sophie.machen@gmail.com>
2023-10-04 19:18:49 -0600

    Initial Commit

---
+++ bookscrool/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bookscrool
+//
+//  Created by Soph Codes on 10/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bookscrool/bookscroolApp.swift
@@ -0,0 +1,17 @@
+//
+//  bookscroolApp.swift
+//  bookscrool
+//
+//  Created by Soph Codes on 10/4/23.
+//
+
+import SwiftUI
+
+@main
+struct bookscroolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
